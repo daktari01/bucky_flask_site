da2b94a769fbaaa85214e477a82e9a76ca8e208f
James Dindi <dindijjames@gmail.com>
2017-12-04 22:27:40 +0300

    [session 4]: HTML Templates

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 # @ signifies a decorator - way to wrap a funtion and modifying its behaviour
 
-@app.route('/')
-def index():
-    return "Method used is %s" % request.method
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("profile.html", name=name)
 
-@app.route('/bacon', methods=['GET', 'POST'])
-def bacon():
-    if request.method == 'POST':
-        return "You are using POST"
-    else:
-        return "You are probably using GET"
+
 
 
 if __name__ == '__main__':
