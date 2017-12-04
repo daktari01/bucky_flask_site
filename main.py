450eb94561d51e4c59978be19442de4907dfd18e
James Dindi <dindijjames@gmail.com>
2017-12-04 22:04:51 +0300

    [session 3]: HTTP Methods

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-from flask import Flask 
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 # @ signifies a decorator - way to wrap a funtion and modifying its behaviour
+
 @app.route('/')
 def index():
-    return 'This is the home page!'
+    return "Method used is %s" % request.method
 
-@app.route('/tuna')
-def tuna():
-    return '<h2>Tuna is awesome!</h2>'
-
-@app.route('/profile/<username>')
-def profile(username):
-    return "<h2>Hey there %s</h2>" % username
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    return "<h2>Post ID is: %s</h2>" % post_id
+@app.route('/bacon', methods=['GET', 'POST'])
+def bacon():
+    if request.method == 'POST':
+        return "You are using POST"
+    else:
+        return "You are probably using GET"
 
 
 if __name__ == '__main__':
