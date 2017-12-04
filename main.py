801f9f4b652dc27cedde055d9e855bedbb6f9229
James Dindi <dindijjames@gmail.com>
2017-12-04 23:08:46 +0300

    [session 7]: Passing objects into templates

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<user>')
-def idex(user=None):
+def index(user=None):
     return render_template("user.html", user=user)
+
+@app.route("/shopping")
+def shopping():
+    food = [
+        "Omena", 
+        "Sukuma wiki", 
+        "Nyama choma", 
+        "Chips mwitu", 
+        "Madondo chafua",
+        "Muturaa sosa"
+        ]
+    return render_template("shopping.html", food=food)
+
 
 
 
